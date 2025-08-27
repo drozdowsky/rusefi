338a6c5a85f8754709d57207ecf0baad559b5db9
GitHub gen-configs Action <action@github.com>
2025-08-27 20:00:31 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_subaru_eg33_f7.h
+++ firmware/controllers/generated/rusefi_generated_subaru_eg33_f7.h
@@ -2068,7 +2068,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.25.subaru_eg33_f7.3736006702"
+#define TS_SIGNATURE "rusEFI master.2025.08.27.subaru_eg33_f7.3736006702"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
